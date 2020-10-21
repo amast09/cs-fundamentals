8908e8da7fd86f924a676be655932c282d80792d
Aaron Mast <amast09@gmail.com>
2020-10-21 06:28:25 -0400

    Add disconnected path to graph path test

--- src/problems/graphHasRoute.test.ts
+++ src/problems/graphHasRoute.test.ts
@@ -41,12 +41,39 @@ describe("graphHasRoute", () => {
   describe("adListGraphHasRoute", () => {
     const adjacencyListGraph: AdjacencyListGraph<number> = new Map();
 
+    const disconnectedPathsToTest = [
+      [1, 7, false],
+      [1, 8, false],
+      [2, 7, false],
+      [2, 8, false],
+      [3, 7, false],
+      [3, 8, false],
+      [0, 7, false],
+      [0, 8, false],
+      [7, 0, false],
+      [7, 1, false],
+      [7, 2, false],
+      [7, 3, false],
+      [7, 7, true],
+      [7, 8, true],
+      [7, 97, false],
+      [8, 0, false],
+      [8, 1, false],
+      [8, 2, false],
+      [8, 3, false],
+      [8, 7, false],
+      [8, 8, true],
+      [8, 97, false],
+    ];
+
     adjacencyListGraph.set(0, new Set([1]));
     adjacencyListGraph.set(1, new Set([2]));
     adjacencyListGraph.set(2, new Set([0]));
     adjacencyListGraph.set(3, new Set([2]));
+    // also including disconnected path 7 --> 8 in this graph
+    adjacencyListGraph.set(7, new Set([8]));
 
-    it.each(testData)(
+    it.each([...testData, ...disconnectedPathsToTest])(
       "for path `%p` -> `%i` should return `%j`",
       (start, end, containsPath) => {
         expect(adListGraphHasRoute(start, end, adjacencyListGraph)).toEqual(
